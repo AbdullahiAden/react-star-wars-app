23ca0b51fd78967d652e57ef4229c2407cbd6c1c
Abdullahi Aden <Abbe6152@gmail.com>
2021-09-23 22:17:49 +0200

    moved fetch to own componenet

--- src/App.js
+++ src/App.js
@@ -1,48 +1,9 @@
-import { useEffect, useState } from "react";
-import { Card, Grid } from "semantic-ui-react";
 import CharactersList from "./components/CharactersList";
 
 function App() {
-  // const [characters, setCharacters] = useState([]);
-
-  // // fetch characters
-  // async function fetchCharacters() {
-  //   const response = await fetch("https://swapi.dev/api/people");
-  //   const data = await response.json();
-  //   setCharacters(data.results);
-  // }
-
-  // useEffect(() => {
-  //   fetchCharacters();
-  // }, []);
-
-  // console.log(characters);
-
   return (
     <>
-      {/* ------------------------------------------- */}
-      <h1>Characters</h1>
-      <Grid columns={3}>
-        {/* loop through characters */}
-
-        {characters.map((character, index) => {
-          return (
-            <Grid.Column key={index}>
-              <Card>
-                <Card.Content>
-                  <Card.Header>{character.name}</Card.Header>
-                  <Card.Description>
-                    <strong>Height</strong>
-                    <p>{character.height}</p>
-                    <strong>Mass</strong>
-                    <p>{character.mass}</p>
-                  </Card.Description>
-                </Card.Content>
-              </Card>
-            </Grid.Column>
-          );
-        })}
-      </Grid>
+      <CharactersList />
     </>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Character = () => {
+const Character = (data) => {
   return <div></div>;
 };
 
