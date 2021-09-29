a9cb25afaf96035bef093a30f9951279db5e2dbd
Abdullahi Aden <Abbe6152@gmail.com>
2021-09-29 16:29:12 +0200

    fixed search input

--- src/App.js
+++ src/App.js
@@ -35,62 +35,147 @@ function App() {
 
   return (
     <>
-      <Navbar characters={characters} />
+      {/* <Navbar data={people} /> */}
 
+      <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+        <button
+          class="navbar-toggler navbar-dark"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarTogglerDemo01"
+          aria-controls="navbarTogglerDemo01"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon  "></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <a class="navbar-brand text-light " href="/">
+            <h2>Star Wars</h2>
+          </a>
+
+          <form class="form-inline my-2 my-lg-0 " action="/searchedChar">
+            <input
+              class="form-control mr-sm-2"
+              type="search"
+              onChange={(e) => setSearchedChar(e.target.value)}
+              placeholder="Search"
+              aria-label="Search"
+            />
+
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
       {/* Rendering characters */}
-      <div className="row">
-        {!people.results ? (
-          <p>Loading</p>
-        ) : (
-          characters.map((character, index) => {
-            return (
-              <div className="col-lg-4  shadow  p-1">
-                {/* if there is no searched character, display all */}
 
-                <h2>
-                  {/* *Make module pop up here, when link is clicked, which shows more  */}
-                  <button
-                    className="btn btn-primary"
-                    type="submit"
-                    onClick={() => {
-                      fetchSingleCharacter(characters[index].url);
-                    }}
-                    data-toggle="modal"
-                    data-target="#exampleModal"
+      {searchedChar == "" ? (
+        <div className="row">
+          {!people.results ? (
+            <p>Loading</p>
+          ) : (
+            characters.map((character, index) => {
+              return (
+                <div className="col-lg-4  shadow  p-1">
+                  {/* if there is no searched character, display all */}
+
+                  <h2>
+                    {/* *Make module pop up here, when link is clicked, which shows more  */}
+                    <button
+                      className="btn btn-primary"
+                      type="submit"
+                      onClick={() => {
+                        fetchSingleCharacter(characters[index].url);
+                      }}
+                      data-toggle="modal"
+                      data-target="#exampleModal"
+                    >
+                      {characters[index].name}
+                    </button>
+
+                    <p>{character.url}</p>
+                  </h2>
+
+                  <div
+                    class="modal fade"
+                    id="exampleModal"
+                    tabindex="-1"
+                    role="dialog"
+                    aria-labelledby="exampleModalLabel"
+                    aria-hidden="true"
                   >
-                    {characters[index].name}
-                  </button>
-
-                  <p>{character.url}</p>
-                </h2>
-                {searchedChar && (
-                  <div>
-                    {searchedChar === character.name ? (
-                      <div>
-                        <button
-                          className="btn btn-primary"
-                          type="submit"
-                          onClick={() => {
-                            fetchSingleCharacter(characters[index].url);
-                          }}
-                          data-toggle="modal"
-                          data-target="#exampleModal"
-                        >
-                          {characters[index].name}
-                        </button>
+                    <div class="modal-dialog" role="document">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title" id="exampleModalLabel">
+                            Character Details
+                          </h5>
+                          <button
+                            type="button"
+                            class="close"
+                            data-dismiss="modal"
+                            aria-label="Close"
+                            // value={character}
+                          >
+                            <span aria-hidden="true">&times;</span>
+                          </button>
+                        </div>
+                        <div class="modal-body">
+                          <div>
+                            <h2>{charDetails.name}</h2>
+                            <strong>height</strong>
+                            <p>{charDetails.height}</p>
+                            <strong>mass</strong>
+                            <p>{charDetails.mass}</p>
+                            <strong>hair color</strong>
+                            <p>{charDetails.hair_color}</p>
+                            <strong>skin color</strong>
+                            <p>{charDetails.skin_color}</p>
+                            <p>{charDetails.url}</p>
+                          </div>
+                        </div>
+                        <div class="modal-footer">
+                          <button
+                            type="button"
+                            class="btn btn-secondary"
+                            data-dismiss="modal"
+                          >
+                            Close
+                          </button>
+                        </div>
                       </div>
-                    ) : (
-                      <p>{searchedChar + "Not Found"}</p>
-                    )}
+                    </div>
                   </div>
-                )}
-
-                <Modal charDetails={charDetails} />
+                </div>
+              );
+            })
+          )}
+        </div>
+      ) : (
+        characters.map((character, index) => {
+          <div>
+            {searchedChar === character.name ? (
+              <div>
+                <button
+                  className="btn btn-primary"
+                  type="submit"
+                  onClick={() => {
+                    fetchSingleCharacter(characters[index].url);
+                  }}
+                  data-toggle="modal"
+                  data-target="#exampleModal"
+                >
+                  {characters[index].name}
+                </button>
               </div>
-            );
-          })
-        )}
-      </div>
+            ) : (
+              <p>{searchedChar + "Not Found"}</p>
+            )}
+          </div>;
+        })
+      )}
 
       {people.previous && (
         <button
